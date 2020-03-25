955eedb936bf3f123b95fe6431f2f4814e1aa0b5
Consuelo Sanna <consu.sanna@gmail.com>
2020-03-25 19:49:51 +0100

    video effect

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React from "react";
 import "./App.css";
 import Particles from "react-particles-js";
 import particleOpt from "./particlesOpt";
+import particlesVideo from "./particles.mp4";
 
 function App() {
   return (
     <div>
-      <h1 align="center">Consuelo Sanna</h1>
+      <video src="./particles.mp4" muted="" autoPlay=""></video>
+      <h1>
+        <span>C</span>
+        <span>onsue</span>lo<span> San</span>na
+      </h1>
       <Particles params={particleOpt} />
     </div>
   );
