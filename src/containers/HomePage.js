289608ecc79786cb7361e1ddca98d0cbfe779283
Consuelo Sanna <consu.sanna@gmail.com>
2020-03-29 13:23:44 +0200

    MyButton and Social comp. style social button

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,68 +1,21 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import Social from "../components/Social";
+import MyButton from "../components/MyButton";
 
 const useStyles = makeStyles(theme => ({
+  root: { marginLeft: 0, marginTop: "20%" },
   paper: {
-    marginTop: theme.spacing(8),
+    marginTop: theme.spacing(2),
     display: "flex",
     flexDirection: "column",
-    alignItems: "center",
-    alignSelf: "left"
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main
-  },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(1)
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2)
+    alignItems: "center"
   }
 }));
-
-function Social() {
-  return (
-    <Grid container>
-      <Grid item xs={6}>
-        <Button type="submit" fullWidth variant="contained" color="primary">
-          Sign In
-        </Button>
-      </Grid>
-      <Grid item xs={6}>
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          color="primary"
-          href="https://material-ui.com/"
-        >
-          Sign In
-        </Button>
-      </Grid>
-      <Grid item xs={4}>
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          color="primary"
-          href="https://material-ui.com/"
-        >
-          <LinkedInIcon fontSize="large" />
-        </Button>
-      </Grid>
-    </Grid>
-  );
-}
 
 function HomePage() {
   const classes = useStyles();
@@ -77,7 +30,8 @@ function HomePage() {
           Hi. I'm a full-stack web developer.
         </Typography>
       </div>
-      <Box mt={8}>{Social()}</Box>
+      <Box mt={2}>{MyButton()}</Box>
+      <Box mt={2}>{Social()}</Box>
     </Container>
   );
 }
