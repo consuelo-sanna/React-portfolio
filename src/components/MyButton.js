65ca4474b43107d1008b6e59656948ddbbacfd40
Consuelo Sanna <consu.sanna@gmail.com>
2020-03-31 18:24:31 +0200

    effect on Main Button with css or withStyle

--- src/components/MyButton.js
+++ src/components/MyButton.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./MyButton.css";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
-// import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
+import { purple } from "@material-ui/core/colors";
 
-/** 
 const useStyles = makeStyles(theme => ({
   container: {
     margin: theme.spacing(3, 0, 2),
@@ -12,29 +13,43 @@ const useStyles = makeStyles(theme => ({
     backgroundColor: "none"
   },
   button: {
-    background: "transparent"
+    background: "black",
+    color: purple[700],
+    fontSize: "1.5em",
+    fontWeight: 700,
+
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+    "&:hover": {
+      background: "#e81bf3",
+      boxShadow: "0 0 25px #e81bf3,0 0 50px #e81bf3,0 0 200px #e81bf3",
+      color: "black",
+      transition: "0.5s"
+    }
   }
 }));
-*/
 
 export default function MyButton() {
-  // const classes = useStyles();
+  const classes = useStyles();
   return (
-    <Grid container>
-      <Grid item xs={6}>
-        <Button type="submit" fullWidth variant="contained" color="primary">
-          Sign In
-        </Button>
-      </Grid>
-      <Grid item xs={6}>
+    <Grid container className={classes.container}>
+      <Grid item xs={5}>
         <Button
           type="submit"
           fullWidth
           variant="contained"
-          color="primary"
-          href="https://material-ui.com/"
+          className={classes.button}
         >
-          Sign In
+          Portfolio
+        </Button>
+      </Grid>
+      <Grid item xs={5}>
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          className={classes.button}
+        >
+          Profile
         </Button>
       </Grid>
     </Grid>
