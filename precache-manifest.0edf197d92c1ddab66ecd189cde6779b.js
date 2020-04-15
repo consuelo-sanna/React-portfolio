00682323ccc7ed3da6bb1b5951ff46d5672e8c0f
Consuelo Sanna <consu.sanna@gmail.com>
2020-04-15 13:24:11 +0200

    Updates

---
+++ precache-manifest.0edf197d92c1ddab66ecd189cde6779b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49e26074ae5fed16a165ef266d4c44f4",
+    "url": "/React-portfolio/index.html"
+  },
+  {
+    "revision": "f3e7f9dcbd49c9da2562",
+    "url": "/React-portfolio/static/css/2.ba989b92.chunk.css"
+  },
+  {
+    "revision": "dbb6e2892b64367c04b6",
+    "url": "/React-portfolio/static/css/main.b850a561.chunk.css"
+  },
+  {
+    "revision": "f3e7f9dcbd49c9da2562",
+    "url": "/React-portfolio/static/js/2.e72fbbfd.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/React-portfolio/static/js/2.e72fbbfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbb6e2892b64367c04b6",
+    "url": "/React-portfolio/static/js/main.30642a5d.chunk.js"
+  },
+  {
+    "revision": "f59a610508fff90213fa",
+    "url": "/React-portfolio/static/js/runtime-main.0ea60777.js"
+  },
+  {
+    "revision": "84aaa5d0a27230c8ab0f6cd28df00c24",
+    "url": "/React-portfolio/static/media/neurospyt.84aaa5d0.jpg"
+  },
+  {
+    "revision": "1d7909a5ac4db20b7d989a713ea2805f",
+    "url": "/React-portfolio/static/media/portfolio.1d7909a5.jpg"
+  },
+  {
+    "revision": "1863ff251b2ac405c439f7007b677a37",
+    "url": "/React-portfolio/static/media/portfoliok.1863ff25.jpg"
+  },
+  {
+    "revision": "757aaef7b2879d0e13ddc526b6b56aa7",
+    "url": "/React-portfolio/static/media/todo.757aaef7.jpg"
+  }
+]);
